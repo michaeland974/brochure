ca613c511169932202039f0a62d409826854e6f9
michaeland974 <micanderson29@gmail.com>
2024-03-20 17:08:31 -0500

    nav bar for mobile and desktop updated

--- src/components/nav.php
+++ src/components/nav.php
@@ -21,9 +21,29 @@
 
 <header>
   <a id="logo" href="./index.php">
-    <img <?php echo "src={$view->get_dir_path()}/svg/garden-logo.svg"?> 
+    <img <?php echo "src={$view->get_dir_path()}/svg/logo.svg"?> 
          alt="Garden Club Logo">
   </a>
+
+  <nav id="desktop-nav-list">
+    <ul>
+      <li>
+        <a href="./index.php">Home</a>
+      </li>
+      <li>
+        <a href="./index.php">About Us</a>
+      </li>
+      <li>
+        <a href="./index.php">Memberships</a>
+      </li>
+      <li>
+        <a href="./index.php">Locations</a>
+      </li>
+      <li>
+        <a href="./contact.php">Contact</a>
+      </li>
+    </ul>
+  </nav>
 
   <button id="toggle-icon"
           aria-controls="mobile-overlay"
